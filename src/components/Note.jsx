88191a92a9a9c711aa888233c9e391c1a216cff5
Engin Büküm <yasengin2005@gmail.com>
2022-09-26 23:18:24 +0300

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is a note</h1>
+      <p>This is a paragraph.</p>
+    </div>
+  );
+}
+
+export default Note;
